2cf5e9be1b7d055f9c198e0ff2ab5563b4949fc1
Octave Crespel <octave.crespel@club.fr>
2021-03-10 19:21:50 +0100

    Simplify LightTrees

--- src/LightTree.cpp
+++ src/LightTree.cpp
@@ -1,17 +1,14 @@
 #include "LightTree.hpp"
 
 RGBColor LightTree::radiance_channel(const LightPathExpression& channel) const {
-    return radiance_channel(channel, channel.length() - 1);
+    return radiance_channel(channel, channel.length() - 1, true);
 }
 
-LightTree::~LightTree() {
+LightTree::LightTree(SurfaceType type, const RGBColor& emitted)
+    : type_(type), emitted_(emitted) {
 }
 
-LightTreeBounce::LightTreeBounce(SurfaceType type)
-    : type_(type) {
-}
-
-RGBColor LightTreeBounce::radiance() const {
+RGBColor LightTree::radiance() const {
     RGBColor out;
     for (size_t i = 0; i < upstream_.size(); i++) {
 	out += brdfs_[i] * angle_cos_[i] * upstream_[i]->radiance() / pdfs_[i];
@@ -20,75 +17,72 @@ RGBColor LightTreeBounce::radiance() const {
 	out /= upstream_.size();
     }
 
+    out += emitted_;
+
     return out;
 }
 
-void LightTreeBounce::add_upstream(const LightTree* tree, float pdf, RGBColor brdf, float angle_cos) {
+void LightTree::add_upstream(const LightTree* tree, float pdf, RGBColor brdf, float angle_cos) {
     upstream_.push_back(tree);
     pdfs_.push_back(pdf);
     brdfs_.push_back(brdf);
     angle_cos_.push_back(angle_cos);
 }
 
-void LightTreeBounce::add_upstream(const LightTree* tree) {
+void LightTree::add_upstream(const LightTree* tree) {
     add_upstream(tree, 1.0f, RGBColor::gray(1.0f), 1.0f);
 }
 
-
-LightTreeBounce::~LightTreeBounce() {
+LightTree::~LightTree() {
     for (const LightTree* tree : upstream_) {
         delete tree;
     }
 }
 
-RGBColor LightTreeBounce::radiance_channel(const LightPathExpression& channel, int offset) const {
-    if (offset <= 0) {
+RGBColor LightTree::radiance_channel(const LightPathExpression& channel, int offset, bool include_emitted) const {
+    if (offset < 0) {
 	return RGBColor();
     }
+
+    RGBColor transmitted;
 
     if (channel[offset] == SurfaceType::REPEAT) {
-	RGBColor out;
 	for (size_t i = 0; i < upstream_.size(); i++) {
-	    out += brdfs_[i] * angle_cos_[i]
-		* (upstream_[i]->radiance_channel(channel, offset - 1)
-		   + upstream_[i]->radiance_channel(channel, offset))
+	    transmitted += brdfs_[i] * angle_cos_[i]
+		* (upstream_[i]->radiance_channel(channel, offset - 1, true)
+		   + upstream_[i]->radiance_channel(channel, offset, false))
 		   / pdfs_[i];
 	}
-	if (upstream_.size() > 0) {
-	    out /= upstream_.size();
-	}
-
-	return out;
-    }
-
-    if (channel[offset] == type_ || channel[offset] == SurfaceType::ANY) {
-	RGBColor out;
+    } else if (channel[offset] == type_ || channel[offset] == SurfaceType::ANY) {
 	for (size_t i = 0; i < upstream_.size(); i++) {
-	    out += brdfs_[i] * angle_cos_[i] * upstream_[i]->radiance_channel(channel, offset - 1) / pdfs_[i];
+	    transmitted += brdfs_[i] * angle_cos_[i] * upstream_[i]->radiance_channel(channel, offset - 1, true) / pdfs_[i];
 	}
-	if (upstream_.size() > 0) {
-	    out /= upstream_.size();
-	}
-
-	return out;
-    }
-
-    return RGBColor();
-}
-
-LightTreeSource::LightTreeSource(const RGBColor& emitted)
-    : emitted_(emitted) {
-}
-
-RGBColor LightTreeSource::radiance() const {
-    return emitted_;
-}
-
-RGBColor LightTreeSource::radiance_channel(const LightPathExpression& channel, int offset) const {
-    if (offset < 0 || channel[offset] != SurfaceType::LIGHT) {
+    } else {
 	return RGBColor();
     }
+    
+    if (upstream_.size() > 0) {
+	transmitted /= upstream_.size();
+    }
 
-    return emitted_;
+    if (include_emitted) {
+	return transmitted + emitted_;
+    } else {
+	return transmitted;
+    }
 }
 
+void LightTree::print(const std::string& suffix) const {
+    std::string more = surface_type_to_char(type_) + suffix;
+    if (type_ == LIGHT && upstream_.size() == 0) {
+	std::cout << more << "\n";
+    } else {
+	for (const LightTree* tree : upstream_) {
+	    tree->print(more);
+	}
+    }
+}
+
+void LightTree::print() const {
+    print("");
+}
--- src/LightTree.hpp
+++ src/LightTree.hpp
@@ -9,44 +9,34 @@
 #include "LightPathExpression.hpp"
 
 class LightTree {
-public:
-    virtual RGBColor radiance() const = 0;
-    virtual RGBColor radiance_channel(const LightPathExpression& channel, int offset) const = 0;
-    RGBColor radiance_channel(const LightPathExpression& channel) const;
-
-    virtual ~LightTree();
-};
-
-class LightTreeBounce : public LightTree {
 private:
     std::vector<const LightTree*> upstream_;
     std::vector<float> pdfs_;
     std::vector<RGBColor> brdfs_;
     std::vector<float> angle_cos_;
-    SurfaceType type_;
 
-    virtual RGBColor radiance_channel(const LightPathExpression& channel, int offset) const;
+    SurfaceType type_;
+    RGBColor emitted_;
+
+    RGBColor radiance_channel(const LightPathExpression& channel, int offset, bool include_emitted) const;
     
     // disable copy constructor & assignment operator
-    LightTreeBounce& operator=(const LightTreeBounce& other);
-    LightTreeBounce(const LightTreeBounce& other);
+    LightTree& operator=(const LightTree& other);
+    LightTree(const LightTree& other);
+    
+    void print(const std::string& prefix) const;
 
 public:
-    LightTreeBounce(SurfaceType type);
-    virtual RGBColor radiance() const;
+    LightTree(SurfaceType type, const RGBColor& emitted);
+    
+    RGBColor radiance() const;
+    RGBColor radiance_channel(const LightPathExpression& channel) const;
     
     void add_upstream(const LightTree* tree, float pdf, RGBColor brdf, float angle_cos);
     void add_upstream(const LightTree* tree);
 
-    ~LightTreeBounce();
+    void print() const;
+
+    ~LightTree();
 };
 
-class LightTreeSource : public LightTree {
-private:
-    RGBColor emitted_;
-    virtual RGBColor radiance_channel(const LightPathExpression& channel, int offset) const;
-    
-public:
-    LightTreeSource(const RGBColor& color);
-    virtual RGBColor radiance() const;
-};
